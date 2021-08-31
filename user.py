2db10e9b14f863cef5e65ac4417b33062aabb308
Nathan Wright <nathanw1214@gmail.com>
2021-08-30 20:14:30 -0600

    chaining commit

--- user.py
+++ user.py
@@ -6,12 +6,13 @@ class User:		# here's what we have so far
     # adding the deposit method
     def make_deposit(self, amount):	# takes an argument that is the amount of the deposit
         self.account_balance += amount	# the specific user's account increases by the amount of the value received
-
+        return self
     def make_withdrawl(self, amount):
         self.account_balance -= amount
-    
+        return self
     def display_user_balance(self):
         print(self.account_balance)
+        return self
 
     def transfer(self,amount,user):
         self.account_balance -= amount
@@ -25,22 +26,10 @@ guido = User('Guido', 'guido@gmail.com')
 monty = User('Monty', 'monty@gmail.com')
 micheal = User('Micheal', 'micheal@gmail.com')
 
-guido.make_deposit(100)
-guido.make_deposit(100)
-guido.make_deposit(100)
-guido.make_withdrawl(50)
-print(guido.account_balance)
+guido.make_deposit(100).make_deposit(100).make_deposit(100).make_withdrawl(50).display_user_balance()
 
-monty.make_deposit(200)
-monty.make_deposit(200)
-monty.make_withdrawl(100)
-monty.make_withdrawl(100)
-print(monty.account_balance)
+monty.make_deposit(200).make_deposit(200).make_withdrawl(100).make_withdrawl(100).display_user_balance()
 
-micheal.make_deposit(500)
-micheal.make_withdrawl(120)
-micheal.make_withdrawl(120)
-micheal.make_withdrawl(120)
-print(micheal.account_balance)
+micheal.make_deposit(500).make_withdrawl(120).make_withdrawl(120).make_withdrawl(120).display_user_balance()
 
 guido.transfer(150, micheal)
